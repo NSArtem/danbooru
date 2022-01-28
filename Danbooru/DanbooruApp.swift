94c29ca1a5b99eeefa3301ef2927ef5720197d10
Artem Abramov <ar.abramov@gmail.com>
2022-01-28 15:53:52 +0200

    Initial Commit

---
+++ Danbooru/DanbooruApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanbooruApp.swift
+//  Danbooru
+//
+//  Created by Artem Abramov on 2022-01-28.
+//
+
+import SwiftUI
+
+@main
+struct DanbooruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
